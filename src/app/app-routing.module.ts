94297725d8344f3c4c8fa8d84e13cf37268653a3
Cristutiu Emanuel <cristutiumanu@gmail.com>
2022-10-02 21:12:48 +0300

    Added Video Functionality using videoJS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ClipService } from './services/clip.service';
 
 const routes: Routes = [
   {
@@ -19,7 +20,9 @@ const routes: Routes = [
   {
     path: 'clip/:id',
     component: ClipComponent,
-    pathMatch: 'full'
+    resolve: {
+      clip: ClipService
+    },
   },
   {
     path: '**',
--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore'
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import IClip from '../models/clip.model';
-import { map, of, switchMap, BehaviorSubject, combineLatest, lastValueFrom } from 'rxjs';
+import { map, of, switchMap, BehaviorSubject, combineLatest, lastValueFrom, Observable } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClipService {
+export class ClipService implements Resolve<IClip | null> {
   public clipsCollection: AngularFirestoreCollection<IClip>;
   pageClips: IClip[] = [];
   pendingRequest: boolean = false;
@@ -16,7 +17,8 @@ export class ClipService {
   constructor(
     private db: AngularFirestore,
     private auth: AngularFireAuth,
-    private storage: AngularFireStorage
+    private storage: AngularFireStorage,
+    private router: Router
   ) {
     this.clipsCollection = db.collection('clips');
   }
@@ -92,5 +94,22 @@ export class ClipService {
     });
 
     this.pendingRequest = true;
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.clipsCollection.doc(route.params.id)
+      .get()
+      .pipe(
+        map(snapshot => {
+        const data = snapshot.data();
+
+        if (!data) {
+          this.router.navigate(['/']);
+          return null;
+        }
+
+        return data;
+      })
+    );
   }
 }
